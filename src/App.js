0ca4c79d37a04c0ad8e047873ae8ecbabe7b35fa
Arthur Welf <arthur.welf@gmail.com>
2019-01-22 12:36:42 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
-      <ProductList />
-      <Details />
-      <Cart />
-      <Default />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 };
