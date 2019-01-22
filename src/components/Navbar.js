97fdf1ee949cf9ff9103541598009a3bcbb26000
Arthur Welf <arthur.welf@gmail.com>
2019-01-22 15:04:35 +0100

    Add Navbar styling with styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import logo from "../logo.svg";
 import { ButtonContainer } from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons
       (Attribution 3.0 Unported); https://www.iconfinder.com/Makoto_msk */}
       <Link to="/">
@@ -15,7 +16,7 @@ const Navbar = () => {
       <ul className="navbar-nav align-items-center">
         <li className="nav-item ml-5">
           <Link to="/" className="nav-link">
-            Products
+            products
           </Link>
         </li>
       </ul>
@@ -27,8 +28,17 @@ const Navbar = () => {
           my cart
         </ButtonContainer>
       </Link>
-    </nav>
+    </NavWrapper>
   );
 };
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 
 export default Navbar;
