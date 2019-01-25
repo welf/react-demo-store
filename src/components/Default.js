713b07040f8079f1f34995a7657d172855dbd38c
Arthur Welf <arthur.welf@gmail.com>
2019-01-25 11:54:16 +0100

    Add 404 page

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-const Default = () => {
+const Default = props => {
+  console.log(props);
   return (
-    <section>
-      <h3>Page not found</h3>
+    <section className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-uppercase text-title pt-5">
+          <h1 className="display-3">404</h1>
+          <h1>error</h1>
+          <h2>page not found</h2>
+          <h4>
+            the requested URL
+            <span className="text-danger">{` ...${
+              props.location.pathname
+            }`}</span>{" "}
+            does not exist
+          </h4>
+        </div>
+      </div>
     </section>
   );
 };
